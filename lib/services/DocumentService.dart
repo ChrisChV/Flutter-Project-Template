bd58ce4c955748211e9d126fe392864e9d4f7d10
Christofer Chavez <xnpiochv@gmail.com>
2020-08-03 23:23:11 -0500

    Se agregó un cambio en documentService que arregla el bug con el cache en iOS

--- lib/services/DocumentService.dart
+++ lib/services/DocumentService.dart
@@ -5,37 +5,58 @@ class DocumentService{
 
   static Future<DocumentSnapshot> getDoc(
       DocumentReference docRef, bool cache, {bool forceServer = false}) async{
-    if(Utils.isOnTest()) return docRef.get();
-    Source source = Source.serverAndCache;
-    bool networkFlag = await Utils.checkNetwork();
-    if(cache || !networkFlag){
-      if(forceServer && !cache) return null;
-      source = Source.cache;
+    try{
+      if(Utils.isOnTest()) return docRef.get();
+      Source source = Source.serverAndCache;
+      bool networkFlag = await Utils.checkNetwork();
+      if(cache || !networkFlag){
+        if(forceServer && !cache) return null;
+        source = Source.cache;
+      }
+      return await docRef.get(source: source);
     }
-    return await docRef.get(source: source);
+    catch(error){
+      if(error.runtimeType == PlatformException) return getDoc(docRef, false);
+      ErrorService.sendError(error);
+      return null;
+    }
   }
 
   static Future<QuerySnapshot> getAll(
       CollectionReference collRef, bool cache, {bool forceServer = false}) async{
-    if(Utils.isOnTest()) return collRef.getDocuments();
-    Source source = Source.serverAndCache;
-    bool networkFlag = await Utils.checkNetwork();
-    if(cache || !networkFlag){
-      if(forceServer && !cache) return null;
-      source = Source.cache;
+    try {
+      if (Utils.isOnTest()) return collRef.getDocuments();
+      Source source = Source.serverAndCache;
+      bool networkFlag = await Utils.checkNetwork();
+      if (cache || !networkFlag) {
+        if (forceServer && !cache) return null;
+        source = Source.cache;
+      }
+      return await collRef.getDocuments(source: source);
     }
-    return await collRef.getDocuments(source: source);
+    catch(error){
+      if(error.runtimeType == PlatformException) return getAll(collRef, false);
+      ErrorService.sendError(error);
+      return null;
+    }
   }
 
   static Future<QuerySnapshot> runQuery(Query query, bool cache, {bool forceServer = false}) async{
-    if(Utils.isOnTest()) return query.getDocuments();
-    Source source = Source.serverAndCache;
-    bool networkFlag = await Utils.checkNetwork();
-    if(cache || !networkFlag){
-      if(forceServer && !cache) return null;
-      source = Source.cache;
+    try {
+      if (Utils.isOnTest()) return query.getDocuments();
+      Source source = Source.serverAndCache;
+      bool networkFlag = await Utils.checkNetwork();
+      if (cache || !networkFlag) {
+        if (forceServer && !cache) return null;
+        source = Source.cache;
+      }
+      return await query.getDocuments(source: source);
     }
-    return await query.getDocuments(source: source);
+    catch(error){
+      if(error.runtimeType == PlatformException) return runQuery(query, false);
+      ErrorService.sendError(error);
+      return null;
+    }
   }
 
   static Stream<QuerySnapshot> getStreamByQuery(Query query){
